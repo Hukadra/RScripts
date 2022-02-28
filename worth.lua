24f63916ebabaf3e8e61109808881d25ac1c9eb5
Hukadra <78614941+Hukadra@users.noreply.github.com>
2022-02-28 21:51:49 +0300

    Create worth.lua

---
+++ worth.lua
@@ -0,0 +1,49 @@
+local vit = {
+       [1] = "LearnSkill",
+       [2] = {
+           ["Skill"] = "Vitality I",
+           ["SkillTreeType"] = "Character"
+       }
+   }
+   
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(vit))
+
+local vitt = {
+       [1] = "LearnSkill",
+       [2] = {
+           ["Skill"] = "Vitality II",
+           ["SkillTreeType"] = "Character"
+       }
+   }
+   
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(vitt))
+
+local vittt = {
+       [1] = "LearnSkill",
+       [2] = {
+           ["Skill"] = "Vitality III",
+           ["SkillTreeType"] = "Character"
+       }
+   }
+   
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(vittt))
+
+local worth = {
+       [1] = "LearnSkill",
+       [2] = {
+           ["Skill"] = "Worthiness I",
+           ["SkillTreeType"] = "Character"
+       }
+   }
+   
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(worth))
+
+local worthh = {
+       [1] = "LearnSkill",
+       [2] = {
+           ["Skill"] = "Worthiness II",
+           ["SkillTreeType"] = "Character"
+       }
+   }
+   
+game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(worthh))
